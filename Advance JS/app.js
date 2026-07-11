cb75191c2c8eaaaedc0bec72b8d3742ab7f58788
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-07-11 06:45:52 +0500

    var , let , const

---
+++ Advance JS/app.js
@@ -0,0 +1,30 @@
+// const user = "Mustafa"
+// console.log(user);
+
+// const user = "Shafay"
+// console.log(user);
+
+
+// console.log(user);
+
+
+// const user = "Mustafa"
+
+
+// if(true){
+// // alert("hi")
+// const user = false
+// }
+
+// console.log(user);
+
+
+const cities = ["karachi" , "lahore" , "Multan" , "peshawar"]
+
+const [biryaniShar , SohanlHawaSher , ChapliKababSher] = cities
+console.log(biryaniShar , "biryaniShar");
+// console.log(PayaSher , "PayaSher");
+console.log(SohanHalwaSher , "SohanHalwaSher");
+console.log(ChapliKababSher , "ChapliKababSher");
+
+console.log(cities);
